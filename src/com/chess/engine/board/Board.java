b4e7f53dc772a894858a5bbbb51ebd2a8cbc876e
Bryan Ortiz <bryanortiz.inbox@gmail.com>
2024-06-19 07:17:33 -0700

    Introduced builder in Board class.

--- src/com/chess/engine/board/Board.java
+++ src/com/chess/engine/board/Board.java
@@ -1,9 +1,54 @@
 package com.chess.engine.board;
 
+import com.chess.engine.Alliance;
+import com.chess.engine.pieces.Piece;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Map;
+import java.util.List;
+
 public class Board {
 
-    public Tile getTile(final int tileCoordinate){
+    private final List<Tile> gameBoard;
+
+    public Board(Builder builder) {
+        this.gameBoard = createGameBoard(builder);
+    }
+
+
+    public Tile getTile(final int tileCoordinate) {
         return null;
     }
 
+    private static List<Tile> createGameBoard(final Builder builder){
+        final Tile[] tiles = new Tile[BoardUtils.NUM_TILES];
+        for(int i = 0; i<BoardUtils.NUM_TILES; i++){
+            tiles[i] = Tile.createTile(i, builder.boardConfig.get(i));
+        }
+        return Collections.unmodifiableList(List.of(tiles));
+    }
+
+    public static class Builder {
+
+        Map<Integer, Piece> boardConfig;
+        Alliance nextMoveMaker;
+
+        public Builder() {
+        }
+
+        public Builder setPiece(final Piece piece) {
+            this.boardConfig.put(piece.getPiecePosition(), piece);
+            return this;
+        }
+
+        public Builder setMoveMaker(final Alliance nextMoveMaker) {
+            this.nextMoveMaker = nextMoveMaker;
+            return this;
+        }
+
+        public Board build() {
+            return new Board(this);
+        }
+    }
 }
